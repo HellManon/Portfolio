d41b43120a797251e6121e860426121c45c3646f
HellManon <manon.hell@outlook.fr>
2025-02-24 16:32:35 +0100

    initialisationdu projet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import catGif from "./assets/chat.gif"
+import catGif2 from "./assets/chat (28).gif"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Mon portfolio</h1>
+     <p>Site en construction</p>
+     <img src={catGif} alt="gif cat 1"></img><br></br>
+     <img src={catGif2} alt="gif cat 2"></img>
     </div>
   );
 }
