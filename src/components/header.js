a272e61d4053bbf23dd789642bd3d26b6c833c15
HellManon <manon.hell@outlook.fr>
2025-03-26 16:20:40 +0100

    correctif responsive design n°2

--- src/components/header.js
+++ src/components/header.js
@@ -6,51 +6,43 @@ import logo from "../images/png-clipart-web-development-web-service-web-develope
 function Header() {
     
     return (
-        <div className="container">
-  <header className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    {/* Container global pour aligner tous les éléments */}
-    <div className="container-fluid d-flex justify-content-between align-items-center">
-      
-      {/* Logo à gauche */}
-      <a className="navbar-brand" href="#">
-      <img src={logo} alt="logo" width="30" height="30" />
-        Manon
-      </a>
+        <header className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div className="container-fluid">
+    
+    {/* Logo et titre à gauche */}
+    <a className="navbar-brand d-flex align-items-center" href="#">
+      <img src={logo} alt="logo" className="logo me-2" />
+      Manon
+    </a>
 
-      {/* Bouton hamburger pour mobile */}
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+    {/* Bouton hamburger pour mobile */}
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
 
-      {/* Liens de navigation au centre */}
-      <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a href="#" className="nav-link">Home</a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-link">Languages</a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-link">Compétences</a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-link">Expériences</a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-link">Projets</a>
-          </li>
-        </ul>
-      </div>
-
-      {/* Bouton LinkedIn à droite */}
-      <a href="https://www.linkedin.com/in/ton-profil" target="_blank" className="btn btn-primary">
-        Contactez-moi
-      </a>
+    {/* Menu centré */}
+    <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+      <ul className="navbar-nav text-center">
+        <li className="nav-item"><a href="#" className="nav-link">Home</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Languages</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Compétences</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Expériences</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Projets</a></li>
+        {/* Bouton LinkedIn dans le menu burger (uniquement en mobile) */}
+        <li className="nav-item mt-3 d-lg-none">
+          <a href="https://www.linkedin.com/in/ton-profil" target="_blank" className="btn btn-primary w-100">
+            Contactez-moi
+          </a>
+        </li>
+      </ul>
     </div>
-  </header>
-</div>
 
+    {/* Bouton LinkedIn visible seulement en desktop */}
+    <a href="https://www.linkedin.com/in/ton-profil" target="_blank" className="btn btn-primary d-none d-lg-inline-block">
+      Contactez-moi
+    </a>
+  </div>
+</header>
 
     )
 }
