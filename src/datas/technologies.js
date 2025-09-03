eba1ac126b4e0141a467a9bcdd8322bee39edb46
HellManon <manon.hell@outlook.fr>
2025-09-03 19:46:56 +0200

    mise à jours des images

--- src/datas/technologies.js
+++ src/datas/technologies.js
@@ -1,24 +1,27 @@
-import javaImg from '../images/technos/java.png';
-import angularImg from '../images/technos/angular.png';
-import nodejsImg from '../images/technos/nodejs.svg';
-import reactImg from '../images/technos/react.png';
-import pythonImg from '../images/technos/python.jpg';
-import mavenImg from '../images/technos/maven.png';
-import junitImg from '../images/technos/junit.png';
-import mockitoImg from '../images/technos/mockito.png';
-import bootstrapImg from '../images/technos/bootstrap.png';
-import githubImg from '../images/technos/github.png';
-import umlImg from '../images/technos/uml.png';
+import javaImg from '../images/technos/java.svg';
+import javascriptImg from '../images/technos/javascript.svg';
+import angularImg from '../images/technos/angular.svg';
+import typescriptImg from '../images/technos/typescript.svg';
+import nodejsImg from '../images/technos/node.svg';
+import reactImg from '../images/technos/react.svg';
+import pythonImg from '../images/technos/python.svg';
+import mavenImg from '../images/technos/maven.svg';
+import junitImg from '../images/technos/junit.svg';
+import mockitoImg from '../images/technos/mockito.svg';
+import bootstrapImg from '../images/technos/bootstrap.svg';
+import githubImg from '../images/technos/git.svg';
+import umlImg from '../images/technos/uml.svg';
+import springImg from '../images/technos/spring.svg';
 
 export const technologies = {
   Languages: [
     { name: 'Java', cover: javaImg },
-    { name: 'JavaScript', cover: reactImg }, // tu peux ajouter un logo JS séparé si tu veux
-    { name: 'TypeScript', cover: angularImg }, // idem pour TS
+    { name: 'JavaScript', cover: javascriptImg }, // tu peux ajouter un logo JS séparé si tu veux
+    { name: 'TypeScript', cover: typescriptImg }, // idem pour TS
     { name: 'Python', cover: pythonImg },
   ],
   Frameworks: [
-    { name: 'Spring', cover: javaImg }, // pas de logo spécifique, sinon ajouter
+    { name: 'Spring', cover: springImg }, // pas de logo spécifique, sinon ajouter
     { name: 'Angular', cover: angularImg },
     { name: 'React', cover: reactImg },
     { name: 'Node.js', cover: nodejsImg },
